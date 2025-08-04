d33b62d8dc6c985357d7a42a69b2dfdfe1984e66
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-08-04 03:06:58 -0300

    xd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/curriculum/', // <-- Añade esto
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
